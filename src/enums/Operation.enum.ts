60da90c1c77734035084f310641988688e902966
leafeea <igorbudenyuk@gmail.com>
2026-01-30 05:08:02 +0100

    fix: replace enum Operation with const object

--- src/enums/Operation.enum.ts
+++ src/enums/Operation.enum.ts
@@ -1,27 +1,29 @@
-export enum Operation{
-  Uppercase = 'uppercase',
-  Lowercase = 'lowercase',
-  CapitalizeWords = 'capitalizeWords',
-  CapitalizeFirst = 'capitalizeFirst',
+export const Operation = {
+  Uppercase: 'uppercase',
+  Lowercase: 'lowercase',
+  CapitalizeWords: 'capitalizeWords',
+  CapitalizeFirst: 'capitalizeFirst',
 
-  PrependPlus = 'prependPlus',
-  RemovePlus = 'removePlus',
-  WrapQuotes = 'wrapQuotes',
-  WrapBrackets = 'wrapBrackets',
-  PrependDash = 'prependDash',
-  PrependDashBracket = 'prependDashBracket',
-  PrependDashQuote = 'prependDashQuote',
+  PrependPlus: 'prependPlus',
+  RemovePlus: 'removePlus',
+  WrapQuotes: 'wrapQuotes',
+  WrapBrackets: 'wrapBrackets',
+  PrependDash: 'prependDash',
+  PrependDashBracket: 'prependDashBracket',
+  PrependDashQuote: 'prependDashQuote',
 
-  CleanSpaces = 'cleanSpaces',
-  CleanTabs = 'cleanTabs',
-  CleanAfterDash = 'cleanAfterDash',
-  SpacesToUnderscore = 'spacesToUnderscore',
-  RemoveSpecial = 'removeSpecial',
-  ReplaceSpecialWithSpace = 'replaceSpecialWithSpace',
+  CleanSpaces: 'cleanSpaces',
+  CleanTabs: 'cleanTabs',
+  CleanAfterDash: 'cleanAfterDash',
+  SpacesToUnderscore: 'spacesToUnderscore',
+  RemoveSpecial: 'removeSpecial',
+  ReplaceSpecialWithSpace: 'replaceSpecialWithSpace',
 
-  Replace = 'replace',
+  Replace: 'replace',
 
-  SortAZ = 'sortAZ',
-  SortZA = 'sortZA',
-  Deduplicate = 'deduplicate',
-}
+  SortAZ: 'sortAZ',
+  SortZA: 'sortZA',
+  Deduplicate: 'deduplicate',
+} as const;
+
+export type Operation = typeof Operation[keyof typeof Operation];
